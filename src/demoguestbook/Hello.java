022a73b27b038ed5330707eb77581a8fd4beed95
Thomas Wionzek <thomas.wionzek@gmail.com>
2012-06-14 19:17:15 -0700

    Implemented Hello.java

--- src/demoguestbook/Hello.java
+++ src/demoguestbook/Hello.java
@@ -1,5 +1,11 @@
 package demoguestbook;
 
 public class Hello {
-
+	/**
+	 * Implement the application
+	 * @param args arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
 }
